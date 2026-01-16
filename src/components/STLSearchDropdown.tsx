1f5da21a771c6668d2caa0289bab278935c6144a
Andy Aragon <hi@andymai.com>
2026-01-16 11:06:27 -0800

    fix(stl-search): improve icon/text vertical alignment

--- src/components/STLSearchDropdown.tsx
+++ src/components/STLSearchDropdown.tsx
@@ -147,8 +147,8 @@ export function STLSearchDropdown({
           aria-expanded={isSingleSite ? undefined : isOpen}
           aria-haspopup={isSingleSite ? undefined : 'menu'}
         >
-          <SearchIcon className="w-4 h-4 flex-shrink-0" />
-          <span>{isSingleSite ? `Search ${enabledSites[0].name}` : 'Find STL'}</span>
+          <SearchIcon className="w-4 h-4 flex-shrink-0 translate-y-px" />
+          <span className="leading-none">{isSingleSite ? `Search ${enabledSites[0].name}` : 'Find STL'}</span>
           {!isSingleSite && <ChevronIcon className="w-3 h-3 flex-shrink-0" isOpen={isOpen} />}
         </button>
       )}
